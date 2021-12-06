a4e7e3078944da1efe5d6f0db7f16732a64c445a
mike <m@home>
2021-12-06 12:10:57 -0500

    update target to https to avoid insecure warning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,7 +28,7 @@ export class AppComponent {
   headerButtons = this.buttons;
   belowButtonText = 'No credit card required.';
   submitSettings = {
-    target: 'http://192.168.1.170:8000/signup', // don't forget to rate-limit this server-side to avoid spam!
+    target: 'https://192.168.1.170:8000/signup', // don't forget to rate-limit this server-side to avoid spam!
     buttonText: 'Sign up!',
     buttonClickAlertText: 'Thanks for signing up!',
     textFieldCssWidth: '10',
