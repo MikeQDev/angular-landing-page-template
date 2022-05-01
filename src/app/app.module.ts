2fcf1c444ac648263b4feb31d7ff631fea8e5c17
mike <m@home>
2022-05-01 10:26:12 -0400

    update email/cta form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ButtonComponent } from './widgets/button/button.component';
 import { DualPaneComponent } from './dual-pane/dual-pane.component';
 import { GridComponent } from './grid/grid.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -20,6 +22,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
     MatButtonModule,
     MatInputModule,
   ],
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { HttpClient } from '@angular/common/http';
 import { Component, HostBinding, Input } from '@angular/core';
 
 @Component({
@@ -19,51 +20,38 @@ export class HeaderComponent {
   @Input() imageAltText = '';
   @Input() belowButtonText = '';
   @Input() submitSettings: any;
+  emailAddress = '';
 
-  performSubmit(event: any): void {
-    // TODO: consider using angular (reactive) form. Just be aware of bundle size increase...
-    // Or move this form iteration to ngOnInit; but beware load time may increase?
-    // Can also getElementById, but this is probably expensive, and conflicts with multiple occurrences
-    let form = undefined;
-    let formTextField = undefined;
-    for (const formElementPath of event.path) {
-      if (formElementPath instanceof HTMLFormElement) {
-        form = formElementPath as HTMLFormElement;
-        for (const childNode of Array.from(form.childNodes)) {
-          if (childNode instanceof HTMLInputElement) {
-            formTextField = childNode;
-            break;
-          }
-        }
-        break;
-      }
-    }
-    const emailAddress = formTextField?.value as string;
-    if (form && formTextField) {
-      // ensure form and field were found
-      if (this.validateEmail(emailAddress)) {
-        form?.submit();
-        form?.reset();
-        if (this.submitSettings.buttonClickAlertText) {
-          // set Timeout else alert will need to be closed before submit executes. Too short a timeout and alert goes away too quick
-          setTimeout(() => {
-            window.alert(this.submitSettings.buttonClickAlertText);
-            window.location = this.submitSettings.location;
-          }, 200);
-        } else {
-          if (this.submitSettings.location) {
-            window.location = this.submitSettings.location;
-          }
-        }
-      } else {
-        window.alert('Please enter a valid email address');
-      }
+  constructor(private http: HttpClient) {}
+
+  performSubmit(): void {
+    if (this.isValidEmail()) {
+      this.http
+        .post<any>(this.submitSettings.target, {
+          emailAddress: this.emailAddress,
+        })
+        .subscribe({
+          next: (data) => {
+            if (this.submitSettings.buttonClickAlertText) {
+              window.alert(this.submitSettings.buttonClickAlertText);
+            }
+            if (this.submitSettings.location) {
+              window.location = this.submitSettings.location;
+            }
+            this.emailAddress = '';
+          },
+          error: (error) => {
+            window.alert('Issue submitting form - please contact admin');
+          },
+        });
     } else {
-      window.alert('Issue submitting email address; please contact admin');
+      window.alert('Please enter a valid email address');
     }
   }
 
-  validateEmail(email: string): boolean {
-    return new RegExp(/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+/).test(email);
+  isValidEmail(): boolean {
+    return new RegExp(/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+/).test(
+      this.emailAddress
+    );
   }
 }
